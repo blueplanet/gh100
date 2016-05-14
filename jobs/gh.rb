161a342d3e27238f751ee783bc6222d9f6fe50e3
blueplanet <erguolinge@gmail.com>
2016-05-14 19:49:07 +0900

    Add github 100

---
+++ jobs/gh.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'faraday'
+
+url = 'https://github.com/blueplanet'
+
+SCHEDULER.every '600s' do
+  doc = Nokogiri::XML(Faraday.get(url).body)
+  days = doc.css('#contributions-calendar .contrib-number').last.text.match /\d*/
+
+  send_event 'last_days', { current: days }
+  send_event 'process', { value: days }
+end
