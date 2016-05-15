efc44379c5eac108622e881b30db9d8acb8ee6e4
blueplanet <erguolinge@gmail.com>
2016-05-15 14:51:33 +0900

    fix init display

--- jobs/gh.rb
+++ jobs/gh.rb
@@ -3,7 +3,7 @@ require 'faraday'
 
 url = 'https://github.com/blueplanet'
 
-SCHEDULER.every '600s' do
+SCHEDULER.every '600s', first_in: 0 do
   doc = Nokogiri::XML(Faraday.get(url).body)
   days = doc.css('#contributions-calendar .contrib-number').last.text.match /\d*/
 
